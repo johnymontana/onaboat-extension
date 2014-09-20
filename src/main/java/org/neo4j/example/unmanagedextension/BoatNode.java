756f8adb5f9f22ceeedf14cadab4c3f51aaf3a1f
George Lesica <george@lesica.com>
2014-09-20 10:26:09 -0600

    Add boat node.

---
+++ src/main/java/org/neo4j/example/unmanagedextension/BoatNode.java
@@ -0,0 +1,54 @@
+package org.neo4j.example.unmanagedextension;
+
+public class BoatNode {
+    public Double lat;
+    public Double lon;
+    public Double x;
+    public Double y;
+    public Double z;
+    public Long timestamp;
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLat() {
+        return this.lat;
+    }
+
+    public void setLon(Double lon) {
+        this.lon = lon;
+    }
+
+    public Double getLon() {
+        return this.lon;
+    }
+
+    public void setX(Double x) {
+        this.x = x;
+    }
+
+    public Double getX() {
+        return this.x;
+    }
+
+    public void setY(Double y) {
+        this.y = y;
+    }
+
+    public Double getY() {
+        return this.y;
+    }
+
+    public void setZ(Double z) {
+        this.z = z;
+    }
+
+    public Double getZ() {
+        return this.z;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
